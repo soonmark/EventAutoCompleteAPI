136a5a5b744f1ad5e8ada20a396635eef9ffef0b
Soonmark <soonmarkella@naver.com>
2018-03-30 11:04:27 +0900

    진짜 새로 만들었음
    기존 세팅 그대로

---
+++ eventAutoCompleteProject/src/main/java/com/soonmark/myapp/DateVO.java
@@ -0,0 +1,112 @@
+package com.soonmark.myapp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class DateVO {
+	String year;
+	String month;
+	String date;
+	String day;
+	String hour;
+	String minute;
+
+	DateVO() {
+		this.year = "-1";
+		this.month = "-1";
+		this.date = "-1";
+		this.day = "-1";
+		this.hour = "-1";
+		this.minute = "-1";
+	}
+
+	DateVO(String year, String month, String date, String day, String hour, String minute) {
+		this.year = year;
+		this.month = month;
+		this.date = date;
+		this.day = day;
+		this.hour = hour;
+		this.minute = minute;
+	}
+	
+	public void set(int idx, String val) {
+		switch(idx) {
+		case 0:
+			this.year = val;
+			break;
+		case 1:
+			this.month = val;
+			break;
+		case 2:
+			this.date = val;
+			break;
+		case 3:
+			this.day = val;
+			break;
+		case 4:
+			this.hour = val;
+			break;
+		case 5:
+			this.minute = val;
+			break;
+		default:
+			break;
+		}
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getMonth() {
+		return month;
+	}
+
+	public void setMonth(String month) {
+		this.month = month;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public String getHour() {
+		return hour;
+	}
+
+	public void setHour(String hour) {
+		this.hour = hour;
+	}
+
+	public String getMinute() {
+		return minute;
+	}
+
+	public void setMinute(String minute) {
+		this.minute = minute;
+	}
+
+	public String toString() {
+		String jsonString = "{\"year\":\"" + this.year + "\", \"month\":\"" + this.month + "\", \"date\":\"" + this.date
+				+ "\", \"day\":\"" + this.day + "\", \"hour\":\"" + this.hour + "\", \"minute\":\"" + this.minute
+				+ "\"}";
+
+		return jsonString;
+	}
+}
